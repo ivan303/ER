dc0c132a6b8ea9e2da54b70da186fff3de4d4041
mmoskwa <mmoskwa@egnyte.com>
2015-08-31 23:04:01 +0200

    Association between employment and schedule

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -2,6 +2,8 @@ class Employment < ActiveRecord::Base
 	belongs_to :doctor
 	belongs_to :clinic
 
+	has_many :schedules
+
 	validates :doctor, presence: true, uniqueness: { scope: :clinic }
 	validates :clinic, presence: true
 end
