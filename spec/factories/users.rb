d76b66f7c9cb0115ac86c68e5d8b896698b9fcc5
mmoskwa <mmoskwa@egnyte.com>
2015-09-08 20:56:35 +0200

    Removes pesel from user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,6 @@ FactoryGirl.define do
     password "12345678"
     firstname { Faker::Name.first_name }
     lastname { Faker::Name.last_name }
-    pesel "12312312312"
     address { Faker::Address.street_address + ', ' +  Faker::Address.city }
   end
 end
