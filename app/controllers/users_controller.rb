fa7a68460190a506d032562d943927587b7a5b64
mmoskwa <mmoskwa@egnyte.com>
2015-09-08 21:01:17 +0200

    Adds simple user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+	before_action :authenticate_user!, only: [:show, :index, :update]
+	load_and_authorize_resource
+	
+	def show
+		# byebug
+	end
+
+	def index
+		@patients = Patient.all
+	end
+
+	def update
+		user = User.find(params[:id])
+		user.update_attributes(user_params)
+		redirect_to users_path
+	end
+
+	private	
+		def user_params
+			params.require(:user).permit(:approved)
+		end
+end
