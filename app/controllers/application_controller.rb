49db9168cc3647b0bf75fdf3e40b159b8a5aa8a6
mmoskwa <mmoskwa@egnyte.com>
2015-09-25 21:44:33 +0200

    Adds method to preven ForbiddenAttributeError because of CanCan gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter do
+    resource = controller_name.singularize.to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
+  
   rescue_from CanCan::AccessDenied do |exception|
     # byebug
     redirect_to user_path(current_user), :alert => exception.message
