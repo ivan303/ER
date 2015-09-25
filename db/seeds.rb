fd9eaf223eed3540501973b1a5fd1130072a51e4
mmoskwa <mmoskwa@egnyte.com>
2015-09-25 21:54:33 +0200

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Creating doctors'
+doctors = []
+5.times do 
+	doctors << FactoryGirl.create(:doctor)
+end
+
+clinics = []
+2.times do
+	clinics << FactoryGirl.create(:clinic)
+end
+
+employments = []
+employments << Employment.create(clinic: clinics[0], doctor: doctors[0])
+employments << Employment.create(clinic: clinics[0], doctor: doctors[1])
+employments << Employment.create(clinic: clinics[0], doctor: doctors[2])
+employments << Employment.create(clinic: clinics[1], doctor: doctors[2])
+employments << Employment.create(clinic: clinics[1], doctor: doctors[3])
+employments << Employment.create(clinic: clinics[1], doctor: doctors[4])
+
+schedules = []
+# byebug
+schedules << Schedule.create(employment_id: employments[1].id, weekday: 'Mon',
+			begins_at: DateTime.new(2015,8,30,9,0,0), ends_at: DateTime.new(2015,8,30,15,0,0))
+schedules << Schedule.create(employment_id: employments[1].id, weekday: 'Tue',
+			begins_at: DateTime.new(2015,8,31,8,0,0), ends_at: DateTime.new(2015,8,31,12,0,0))
+schedules << Schedule.create(employment_id: employments[1].id, weekday: 'Tue',
+			begins_at: DateTime.new(2015,8,31,15,0,0), ends_at: DateTime.new(2015,8,31,19,0,0))
+
+schedules << Schedule.create(employment_id: employments[2].id, weekday: 'Mon',
+			begins_at: DateTime.new(2015,8,30,9,0,0), ends_at: DateTime.new(2015,8,30,15,0,0))
+schedules << Schedule.create(employment_id: employments[2].id, weekday: 'Tue',
+			begins_at: DateTime.new(2015,8,31,9,0,0), ends_at: DateTime.new(2015,8,31,15,0,0))
+schedules << Schedule.create(employment_id: employments[3].id, weekday: 'Mon',
+			begins_at: DateTime.new(2015,8,30,17,0,0), ends_at: DateTime.new(2015,8,30,19,0,0))
+schedules << Schedule.create(employment_id: employments[3].id, weekday: 'Wed',
+			begins_at: DateTime.new(2015,9,1,9,0,0), ends_at: DateTime.new(2015,9,1,15,0,0))
