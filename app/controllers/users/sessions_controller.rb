25aa828fec27b809d1933b024f85884864d78abc
mmoskwa <mmoskwa@egnyte.com>
2015-10-13 23:44:27 +0200

    Fixes logging in proccess

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,20 +1,26 @@
 class Users::SessionsController < Devise::SessionsController
+	include ApplicationHelper
 	def create
 		user = User.find_by(email: params[:user][:email])
 		if user
-			if user.role == 'Patient'
-				if user.approved?
+			if user.valid_password?(params[:user][:password])
+				if user.role == 'Patient'
+					if user.approved?
+						sign_in user
+						flash[:success] = I18n.t("devise.sessions.signed_in")
+						redirect_to user_path(current_user)
+					else
+						flash[:alert] = I18n.t("devise.failure.not_approved")
+						redirect_to new_user_session_path
+					end
+				else
 					sign_in user
 					flash[:success] = I18n.t("devise.sessions.signed_in")
 					redirect_to user_path(current_user)
-				else
-					flash[:alert] = I18n.t("devise.failure.not_approved")
-					redirect_to new_user_session_path
 				end
 			else
-				sign_in user
-				flash[:success] = I18n.t("devise.sessions.signed_in")
-				redirect_to user_path(current_user)
+				flash[:error] = I18n.t("devise.failure.invalid")
+				redirect_to new_user_session_path
 			end
 		else
 			flash[:error] = I18n.t("devise.failure.not_found_in_database")
