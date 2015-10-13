a9d8eebec5da61b5c5594d8e4386d1ba50eaa325
mmoskwa <mmoskwa@egnyte.com>
2015-10-13 23:59:46 +0200

    Action for creating new clinic

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -5,16 +5,34 @@ class ClinicsController < ApplicationController
 
 	def index
 		@clinics = Clinic.all.order(:name)
+		@new_clinic = Clinic.new
 	end
 
 	def destroy
-		byebug
 		clinic = Clinic.find(params[:id])
-		if clinic # DELETE
+		if clinic.destroy
 			flash_message :success,  I18n.t("clinics.destroy.success")
 			redirect_to clinics_path
 		else
-			flash_message :error, "nie udało sie" #TODO
+			flash_message :error, I18n.t("clinics.destroy.error")
 		end
 	end
+
+	def create
+		clinic = Clinic.new(clinic_params)
+		if clinic.save
+			flash_message :success, I18n.t("clinics.create.success")
+			redirect_to clinics_path
+		else
+			flash_message :error, I18n.t("clinics.create.error")
+			redirect_to clinics_path
+		end
+	end
+
+	private
+		def clinic_params
+			params.require(:clinic).permit(:name)
+		end
+
+
 end
