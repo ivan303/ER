59d51e7310993dafdc74493ea430c38e8029738a
mmoskwa <mmoskwa@egnyte.com>
2015-10-14 00:15:14 +0200

    Show action for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,24 @@ class UsersController < ApplicationController
 	load_and_authorize_resource
 	
 	def show
+		Appointment.remove_unconfirmed_appointments
+		if current_user.is_admin?
+			@appointments = Appointment.all.order(:begins_at).reverse
+		elsif current_user.is_doctor?
+			employments = Employment.where(doctor_id: current_user.id)
+			schedules = []
+			employments.each do |employment|
+				schedules << Schedule.where(employment_id: employment.id)
+			end
+			schedules.flatten!
+			@appointments = []
+			schedules.each do |schedule|
+				@appointments << Appointment.where(schedule_id: schedule.id).where.not(confirmed_at: nil)
+			end
+			@appointments.flatten!
+		elsif current_user.is_patient?
+			@appointments = Appointment.where(patient_id: current_user.id)
+		end
 	end
 
 	def index
