98b891747b1c592f502cd7100de75a78bfa9e5f5
mmoskwa <mmoskwa@egnyte.com>
2015-10-13 23:42:33 +0200

    Some model functions; errors messages

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,11 @@ class Appointment < ActiveRecord::Base
 	belongs_to :schedule
 	belongs_to :patient
 
+	attr_accessor :doctor_id
+	attr_accessor :clinic_id
+	attr_accessor :weekday
+	attr_accessor :date
+
 	validates :schedule_id, presence: true
 	validates :patient_id, presence: true
 	validates :begins_at, presence: true
@@ -17,6 +22,22 @@ class Appointment < ActiveRecord::Base
 
 	def comparable_ends_at_format
 		self.ends_at.strftime('%H%M').sub(/^0{,3}/, "").to_i unless self.ends_at.nil?
+	end
+
+	def self.remove_unconfirmed_appointments
+		now = DateTime.now
+		appointments = Appointment.all
+		byebug
+		appointments.each do |appointment|
+			# if appointment[:confirmed_at] == nil
+			unless appointment[:confirmed_at]
+				byebug
+				if appointment.created_at + 10.minutes < now.utc()
+					byebug
+					appointment.destroy
+				end
+			end
+		end
 	end
 
 	validate :doctor_works_in_clinic_in_visit_time
@@ -35,14 +56,14 @@ class Appointment < ActiveRecord::Base
 	def doctor_works_in_clinic_in_visit_time
 		if self.schedule_id and Schedule.find(self.schedule_id)
 			schedule = Schedule.find(schedule_id)
-			unless begins_at.strftime('%a') == schedule.begins_at.strftime('%a')
-				errors.add("visit", "doctor doesn't work in clinic that day")
+			unless begins_at.strftime('%a') == schedule.weekday
+				errors.add("visit", "not_work_that_day")
 				return false
 			end
 
 			unless self.comparable_begins_at_format >= schedule.comparable_begins_at_format and
 				self.comparable_ends_at_format <= schedule.comparable_ends_at_format
-				errors.add("visit", "doctor doesn't work in clinic in that time")
+				errors.add("visit", "not_work_that_time")
 				return false
 			end
 		end
@@ -51,7 +72,7 @@ class Appointment < ActiveRecord::Base
 	def doctor_is_free_in_visit_time
 		if self.begins_at and self.ends_at and self.schedule_id
 			if Appointment.where(schedule_id: self.schedule_id, begins_at: self.begins_at, ends_at: self.ends_at).exists?
-				errors.add("visit", "doctor has visit in this time")
+				errors.add("visit", "another_visit")
 				return false
 			end
 		end
@@ -61,16 +82,16 @@ class Appointment < ActiveRecord::Base
 		if self.begins_at and self.ends_at
 			if self.begins_at.strftime('%H') == self.ends_at.strftime('%H')
 				unless self.begins_at.strftime('%M') == '00' and self.ends_at.strftime('%M') == '30'
-					errors.add("visit", "visit doesn't last half hour")
+					errors.add("visit", "not_half_hour")
 					return false
 				end
 			elsif self.begins_at.strftime('%H').to_i + 1 == self.ends_at.strftime('%H').to_i
 				unless self.begins_at.strftime('%M') == '30' and self.ends_at.strftime('%M') == '00'
-					errors.add("visit", "visit doesn't last half hour")
+					errors.add("visit", "not_half_hour")
 					return false
 				end
 			else
-				errors.add("visit", "visit doesn't last half hour")
+				errors.add("visit", "not_half_hour")
 				return false
 			end
 		end
@@ -79,9 +100,26 @@ class Appointment < ActiveRecord::Base
 	def visit_begins_and_ends_the_same_day
 		if self.begins_at and self.ends_at
 			unless self.begins_at.strftime('%d%m%Y') == self.ends_at.strftime('%d%m%Y')
-				errors.add("visit", "visit should begin and end the same day")
+				errors.add("visit", "begins_ends_one_day")
 				return false
 			end
+		end
+	end
+
+	def can_delete?
+		byebug
+		if self.begins_at > DateTime.now.utc()
+			true
+		else
+			false
+		end
+	end
+
+	def can_confirm?
+		if self.created_at + 10.minutes > DateTime.now.utc()
+			true
+		else
+			false
 		end
 	end
 end
