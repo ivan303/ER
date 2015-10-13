f5be0ac21fb70e896791049f3107130e15ac2b10
mmoskwa <mmoskwa@egnyte.com>
2015-10-14 00:30:59 +0200

    New routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,18 @@ Rails.application.routes.draw do
                     sessions: "users/sessions" }
   resources :users, only: [:show, :index, :update]
   resources :schedules, only: [:show, :index, :destroy, :create]
-  resources :clinics, only: [:index, :destroy]
+  resources :clinics, only: [:index, :destroy, :create]
+  resources :doctors, only: [:index, :update, :destroy, :create]
+  resources :patients, only: [:index, :update, :destroy]
+  resources :employments, only: [:index, :destroy, :create]
   # resources :appointments, only: [:index]
 
   # map.resources :appointments, :collection => { :update_calendar => :get } 
-  resources :appointments, only: [:index] do
+  resources :appointments, only: [:index, :create, :destroy] do
     get 'update_calendar', :on => :collection
+    post 'create_appointment_in_clinic', :on => :collection
+    post 'create_appointment_at_doctor', :on => :collection
+    put 'confirm', :on => :member
   end
 
 
@@ -19,6 +25,6 @@ Rails.application.routes.draw do
 
   # match 'user_root' => 'users#show'
   # get 'users#show', :as => :user_root
-  root 'users#index'
+  root 'users#show'
 
 end
